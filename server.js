d2c9ccb40aa3d6bf280312edeb56b57aa6ecd4ca
Stanley <stanley.wilder.24@gmail.com>
2022-04-19 21:39:30 -0700

    lab 07

---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+// Requires
+const express = require('express');
+require('dotenv').config();
+let weatherData = require(`./weather.json`);
+
+
+// Use
+const app = express();
+const PORT = process.env.PORT || 3002;
+// if something is wrong 3002 with run with the server.
+
+
+
+
+// Routes
+app.get('/', (request, response) => {
+  response.send('HI');
+});
+
+
+
+
+
+
+
+
+
+app.get('*', (resquest , response) => {
+  response.send('Its not here!!')
+)};
+// Errors
+
+
+
+
+
+// Listen
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+
